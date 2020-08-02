7de30e2fb59d103575486061bd5f3644ac67b33b
Kyle McCurley <kmccurley123@gmail.com>
2020-08-01 22:24:14 -0400

    Update cute_angles.js

--- cute_angles.js
+++ cute_angles.js
@@ -1,23 +1,26 @@
-function dms(num) {
+function dms(degreesFloat) {
   let degrees = Math.floor(num);
   let minutes = calculateMinutes();
   let seconds = calculateSeconds();
-  let output = String(degrees + '˚' + minutes + '\'' + seconds + '"');
+  return String(degrees + '˚' + minutes + '\'' + seconds + '"');
 
   function calculateMinutes() {
-    let calc = Math.floor(60 * (num - degrees));
+    const MINUTES_PER_DEGREE = 60;
+
+    let calc = Math.floor(MINUTES_PER_DEGREE * (num - degrees));
     return (String(calc).length < 2 ? ('0' + calc) : calc);
   }
 
   function calculateSeconds() {
-    let calc = Math.floor(60 * (60 * (num - degrees) - minutes));
+    const SECONDS_PER_MINUTE = 60;
+    let calc = Math.floor(SECONDS_PER_MINUTE * (SECONDS_PER_MINUTE * ((num - degrees) - minutes)));
     return (String(calc).length < 2 ? ('0' + calc) : calc);
   }
 }
 
-dms(30);           // 30°00'00"
-dms(76.73);        // 76°43'48"
-dms(254.6);        // 254°35'59"
-dms(93.034773);    // 93°02'05"
+console.log(dms(30));           // 30°00'00"
+console.log(dms(76.73));        // 76°43'48"
+console.log(dms(254.6));        // 254°35'59"
+console.log(dms(93.034773));    // 93°02'05"
 dms(0);            // 0°00'00"
 dms(360);          // 360°00'00" or 0°00'00"
