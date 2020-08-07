c0f01b7d4ef0201af4b080eb28eb159441ae8f65
Kyle McCurley <kmccurley123@gmail.com>
2020-08-07 12:26:22 -0400

    Create transactions.js

---
+++ transactions.js
@@ -0,0 +1,26 @@
+function transactionsFor(inventoryItem, transactions) {
+  let result = [];
+  let filter = (_, idx) => {
+    let transaction = transactions[idx];
+    return transaction.id === inventoryItem;
+  }
+
+  return transactions.filter(filter);
+}
+
+let transactions = [ { id: 101, movement: 'in',  quantity:  5 },
+                     { id: 105, movement: 'in',  quantity: 10 },
+                     { id: 102, movement: 'out', quantity: 17 },
+                     { id: 101, movement: 'in',  quantity: 12 },
+                     { id: 103, movement: 'out', quantity: 20 },
+                     { id: 102, movement: 'out', quantity: 15 },
+                     { id: 105, movement: 'in',  quantity: 25 },
+                     { id: 101, movement: 'out', quantity: 18 },
+                     { id: 102, movement: 'in',  quantity: 22 },
+                     { id: 103, movement: 'out', quantity: 15 }, ];
+
+transactionsFor(101, transactions);
+// returns
+// [ { id: 101, movement: "in",  quantity:  5 },
+//   { id: 101, movement: "in",  quantity: 12 },
+//   { id: 101, movement: "out", quantity: 18 }, ]
